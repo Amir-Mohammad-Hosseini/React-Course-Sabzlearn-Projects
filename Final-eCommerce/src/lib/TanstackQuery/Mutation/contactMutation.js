6457ab877ff959f5c968dd458c840bfbc2b6b3fb
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-04 19:45:57 +0330

    install tanstack and create tanstack folder and add queryclient and add contactMutation

---
+++ Final-eCommerce/src/lib/TanstackQuery/Mutation/contactMutation.js
@@ -0,0 +1,21 @@
+import { toast } from "sonner"
+import { sendContactForm } from "../../../Api/Contact/sendContactForm"
+
+export const contactMutation = () => {
+    return (
+        {
+            mutationFn :sendContactForm ,
+            onMutate : () => {
+                toast.loading("درحال ارسال پیام...")
+            },
+            onSuccess : () => {
+                toast.dismiss()
+                toast.success("پیام با موفقیت ارسال شد")
+            },
+            onError : (error) => {
+                toast.dismiss()
+                toast.error("خطا در ارسال پیام به سرور")
+            },
+        }
+    )
+}
