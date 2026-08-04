ee4a20f0b016303742c6d1484796d264d7411660
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-04 19:43:55 +0330

    add react-query provider component

--- Final-eCommerce/src/main.jsx
+++ Final-eCommerce/src/main.jsx
@@ -4,9 +4,13 @@ import "./../public/css/index.css";
 import "./../public/css/fonts.css";
 import { RouterProvider } from "react-router";
 import router from "./lib/routes.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./lib/TanstackQuery/queryClient.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </QueryClientProvider>,
 );
